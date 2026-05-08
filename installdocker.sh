e1fe43d7b8c069989f70f5c9a5a2e9c0d2845661
virgiliodelacruzjardon-mongodb <virgilio.delacruzjardon@mongodb.com>
2026-05-08 13:48:45 -0600

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -16,5 +16,5 @@ Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
