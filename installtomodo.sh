2a059b3df12c92d8338486419ad916f7f9360b97
virgiliodelacruzjardon-mongodb <virgilio.delacruzjardon@mongodb.com>
2026-05-08 13:32:39 -0600

    Update installtomodo.sh

--- installtomodo.sh
+++ installtomodo.sh
@@ -2,6 +2,7 @@ mongod --shutdown --config /etc/mongod.conf
 sleep 3
 python3 -m venv .
 source bin/activate
+python3 -m pip install "typer==0.9.0" "click==8.1.8"
 python3 -m pip install pymongo 
 python3 -m pip install tomodo
 tomodo provision sharded --shards 3
